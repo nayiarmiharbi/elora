aa20fb0e09b7b759296cf34a150620dd3c45536a
nayiarmiharbi <miharbinayiar@gmail.com>
2025-05-10 15:57:10 +0600

    add field to userDetails

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -6,16 +6,22 @@ import { useNavigate } from 'react-router-dom';
 function CreateUser() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
   const navigate = useNavigate();
   const userCollectionRef = collection(db, "userDetails");
 
   const createUser = async () => {
     if (!firstName || !lastName) return alert("Please fill both fields.");
-    await addDoc(userCollectionRef, { firstName, lastName });
+    await addDoc(userCollectionRef, { firstName, lastName, email, phone, address });
+    alert("User created successfully");
     navigate('/');
   };
 
   return (
+    console.log(email),
+    console.log(address),
     <div>
       <h2>Create User</h2>
       <input
@@ -27,6 +33,21 @@ function CreateUser() {
         placeholder="Last Name"
         value={lastName}
         onChange={(e) => setLastName(e.target.value)}
+      /><br /><br />
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+      <input
+        placeholder="Phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      /><br /><br />
+      <input
+        placeholder="Address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
       /><br /><br />
       <button onClick={createUser}>Submit</button>
     </div>
